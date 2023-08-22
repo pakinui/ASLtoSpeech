9e5c581cd214ab68df16424240849778d8726462
Laura Yoo <yoola217@student.otago.ac.nz>
2023-08-23 00:59:33 +1200

    hopefully i made all the merge of my progress

--- app/345Application/camera.h
+++ app/345Application/camera.h
@@ -65,9 +65,9 @@ private:
     bool m_applicationExiting = false;
     bool m_doImageCapture = true;
 
-public slots:
+public Q_SLOTS:
 
-private slots:
+private Q_SLOTS:
     void displayCameraError();
     void updateCameraDevice(QAction *action);
     void updateCameras();
