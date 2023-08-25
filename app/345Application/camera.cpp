073d49bbcf50e2dd8375cab12d5fe209bf78bff2
Laura Yoo <yoola217@student.otago.ac.nz>
2023-08-26 00:03:08 +1200

    progress so far (not working)

--- app/345Application/camera.cpp
+++ app/345Application/camera.cpp
@@ -121,27 +121,69 @@ Camera::Camera() : ui(new Ui::Camera)
 
 // This slot is called when a new frame is captured.
 void Camera::imageAvailable(QVideoFrame frame) {
-    //qDebug() << count << "not working";
-    if(count == 1){
-        old_frame = frame;
+    const uchar *frame_data = frame.bits(2);
+    int height = frame.height();
+    int width = frame.width();
+    int c = frame.planeCount();
+    qDebug() << c;
+    Py_Initialize();
+    QString currentPath = QDir::currentPath();
+    QString newPath = currentPath + "/../345Application";
+    std::string pythonCode = "import sys; sys.path.append('"+ newPath.toStdString() +"')";
+    PyRun_SimpleString(pythonCode.c_str());
+    PyObject* pModule = PyImport_ImportModule("inference_classifier");
+    if(!pModule){
+        PyErr_Print();
+    }else{
+        qDebug() << "yo";
     }
-    if(count%20 == 0){
-        qDebug() << count;
 
-        //QImage image = frame.toImage();
-        QString str = "hello " + QString::number(count);;
-        sink->setSubtitleText(str);
-        // Create a Python object that wraps the C++ object.
-        //PySide6.QtMultimedia.QVideoSink video_sink = new PySide6.QtMultimedia.QVideoSink(video_sink_cpp);
 
-        // Pass the Python object to the Python.
-        //python_code.video_sink = video_sink;
-
-        sink->setVideoFrame(old_frame);
-        old_frame = frame;
-        count++;
+    PyObject *pFunc = PyObject_GetAttrString(pModule, "overlay");
+    if(!pFunc){
+        PyErr_Print();
+    }else{
+        qDebug() << "yo2";
     }
-    count++;
+    qDebug() << "hi";
+    // Create a flat C array to hold the image data
+    size_t frame_size = height * width * 3;
+    qDebug() << "hi";
+    unsigned char *flat_array = new unsigned char[frame_size];
+    qDebug() << "hi";
+    qDebug() << "hi";
+    // Convert flat array into a Python bytes object
+    PyObject *pBytes = PyBytes_FromStringAndSize(reinterpret_cast<const char *>(flat_array), frame_size);
+    qDebug() << "hi";
+    if(!pBytes){
+        PyErr_Print();
+    }else{
+        qDebug() << pBytes;
+    }
+
+    PyObject *pValue = PyObject_CallFunctionObjArgs(pFunc, pBytes, NULL);
+    if(!pValue){
+        qDebug() << "?";
+        PyErr_Print();
+    }else{
+        qDebug() << "yo3";
+    }
+
+
+
+    //QImage image = frame.toImage();
+    QString resultString = QString::fromUtf8(PyUnicode_AsUTF8(pValue));
+    Py_Finalize();
+    QString str = "hello " + resultString;
+    sink->setSubtitleText(str);
+    // Create a Python object that wraps the C++ object.
+    //PySide6.QtMultimedia.QVideoSink video_sink = new PySide6.QtMultimedia.QVideoSink(video_sink_cpp);
+
+    // Pass the Python object to the Python.
+    //python_code.video_sink = video_sink;
+
+    sink->setVideoFrame(frame);
+
 // below saves the frames as images in /captures
 // currently just overwrites the old image with the current frame
 // but it can save each frame individually
@@ -158,7 +200,6 @@ void Camera::imageAvailable(QVideoFrame frame) {
 
 //    // Generate a unique filename using the current time.
 //    QString filename = QString::number(count);
-////    count += 1;
 
 //    QString currentPath = QDir::currentPath();
 //    QString newPath = currentPath + "/../345Application";
--- app/345Application/inference_classifier.py
+++ app/345Application/inference_classifier.py
@@ -1,33 +1,12 @@
 import os
-import sys
-import time
 import mediapipe as mp
 import numpy as np
 import pickle
 import cv2
-from PySide6.QtCore import Qt, QThread, Signal, Slot
-from PySide6.QtGui import QAction, QImage, QKeySequence, QPixmap
-from PySide6.QtWidgets import (QApplication, QComboBox, QGroupBox,
-                               QHBoxLayout, QLabel, QMainWindow, QPushButton,
-                               QSizePolicy, QVBoxLayout, QWidget)
 
-import PySide6.QtMultimedia
-from PySide6.QtMultimedia import QVideoSink
 
 model_dict = pickle.load(open('ASL_model.p', 'rb'))
 model = model_dict['model']
-
-# Create a Python object that will listen for the signal.
-video_sink = QVideoSink()
-
-# Connect the signal to the Python object.
-video_sink.imageAvailable.connect(video_sink.renderFrame)
-
-
-cap = cv2.VideoCapture(0)
-
-def print_hello():
-    print("Hello from Python module!")
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -35,84 +14,52 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
+print("hello")
+def overlay(frame_data):
+    print("hi")
+    flat_array = bytearray(frame_data)
+    height = 720
+    width = 1280
+    frame_np = np.frombuffer(flat_array, dtype=np.uint8).reshape(height, width, 3)
+    data_aux = []
+    x_ = []
+    y_ = []
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+    frame = cv2.flip(frame_np, 1)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    print("did it work?")
+    results = hands.process(frame_rgb)
+    if results.multi_hand_landmarks:
+        num_detected_hands = len(results.multi_hand_landmarks)
 
-#def overlay(frame):
-#    while True:
+        for hand_landmarks in results.multi_hand_landmarks:
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
 
-#        data_aux = []
-#        x_ = []
-#        y_ = []
+                x_.append(x)
+                y_.append(y)
 
-#        ret, frame = cap.read()
-#        frame = cv2.imread('.\captures\1.jpg')
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
 
-#        H, W, _ = frame.shape
+        if num_detected_hands == 1:
+            # If only one hand is detected, duplicate the data_aux
+            data_aux += data_aux
 
-#    frame = cv2.flip(frame, 1)
-#    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        x1 = int(min(x_) * W) - 10
+        y1 = int(min(y_) * H) - 10
 
-#    results = hands.process(frame_rgb)
-#    if results.multi_hand_landmarks:
-#        num_detected_hands = len(results.multi_hand_landmarks)
+        x2 = int(max(x_) * W) - 10
+        y2 = int(max(y_) * H) - 10
 
-#        # for hand_landmarks in results.multi_hand_landmarks:
-#        #     mp_drawing.draw_landmarks(
-#        #         frame,  # image to draw
-#        #         hand_landmarks,  # model output
-#        #         mp_hands.HAND_CONNECTIONS,  # hand connections
-#        #         mp_drawing_styles.get_default_hand_landmarks_style(),
-#        #         mp_drawing_styles.get_default_hand_connections_style())
+        prediction = model.predict([np.asarray(data_aux)])
+        predicted_class_label = prediction[0]
 
-#        for hand_landmarks in results.multi_hand_landmarks:
-#            for i in range(len(hand_landmarks.landmark)):
-#                x = hand_landmarks.landmark[i].x
-#                y = hand_landmarks.landmark[i].y
-
-#                x_.append(x)
-#                y_.append(y)
-
-#            for i in range(len(hand_landmarks.landmark)):
-#                x = hand_landmarks.landmark[i].x
-#                y = hand_landmarks.landmark[i].y
-#                data_aux.append(x - min(x_))
-#                data_aux.append(y - min(y_))
-
-#        if num_detected_hands == 1:
-#            # If only one hand is detected, duplicate the data_aux
-#            data_aux += data_aux
-
-#        x1 = int(min(x_) * W) - 10
-#        y1 = int(min(y_) * H) - 10
-
-#        x2 = int(max(x_) * W) - 10
-#        y2 = int(max(y_) * H) - 10
-
-#        prediction = model.predict([np.asarray(data_aux)])
-#        predicted_class_label = prediction[0]
-
-#        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-#        cv2.putText(frame, predicted_class_label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
-#                    cv2.LINE_AA)
-
-#    out.write(frame)
-
-#    cv2.imshow('frame', frame)
-#    if cv2.waitKey(1) == ord('q'):
-#        break
-
-#cap.release()
-#out.release()
-#cv2.destroyAllWindows()
-
-def overlayText():
-    # Get the text to overlay.
-    text = "This is some text."
-    frame = cv2.imread('.\captures\1.jpg')
-
-
-    # Return the frame with the text overlayed.
-    return "String"
+        return predicted_class_label
+    else:
+        return "N/A"
 
