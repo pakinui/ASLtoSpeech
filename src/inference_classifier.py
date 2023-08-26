72fb98010383bf009fe1d734885f5bdfb276631d
LauraYoo990914 <80384500+LauraYoo990914@users.noreply.github.com>
2023-08-27 11:34:20 +1200

    Updated inference_classifier.py with comments

--- src/inference_classifier.py
+++ src/inference_classifier.py
@@ -26,33 +26,39 @@ class PythonVideoSink(PySide6.QtCore.QObject):
   def videoFrame(self, psink):
     print("changed")
 
+# Class to collect necessary modules to execute sign-langue detection on frame(=image)
 class PythonTest:
 
 
     def __init__(self):
 
+      # Declear the current frame of the webcam
         self.img_path = "../../resources/captures/output.jpg"
 
+      # Load the trained sign-lange detection model
         self.model_dict = pickle.load(open('ASL_model.p', 'rb'))
         self.model =self.model_dict['model']
 
+      # Declare opencv 
         self.cap = cv2.VideoCapture(0)
 
+      # Declare necessary functionalities from Mediapipe module
         self.mp_hands = mp.solutions.hands
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_drawing_styles = mp.solutions.drawing_styles
 
         self.hands = self.mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-
+      # Setting opencv 
         self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
         self.out = cv2.VideoWriter('output.avi', self.fourcc, 20.0, (640, 480))
 
-
+    # Testing method
     def print_hello():
             print("Hello from Python module!")
             return "heyyyy"
-
+    
+    # Sign-language detection method
     def sign_identifier(self):
 
          frame = cv2.imread("../../resources/captures/output.jpg")
@@ -63,20 +69,16 @@ class PythonTest:
 
          H, W, _ = frame.shape
 
+        # Modify the cv to be fliped % set frame rgb
          frame = cv2.flip(frame, 1)
          frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+        # Declare variable of the hand which has been detected on the frame
          results = self.hands.process(frame_rgb)
+
+        # If statement to place landmarks of fingers(if the hand was detected)
          if results.multi_hand_landmarks:
              num_detected_hands = len(results.multi_hand_landmarks)
-
-             # for hand_landmarks in results.multi_hand_landmarks:
-             #     mp_drawing.draw_landmarks(
-             #         frame,  # image to draw
-             #         hand_landmarks,  # model output
-             #         mp_hands.HAND_CONNECTIONS,  # hand connections
-             #         mp_drawing_styles.get_default_hand_landmarks_style(),
-             #         mp_drawing_styles.get_default_hand_connections_style())
 
              for hand_landmarks in results.multi_hand_landmarks:
                  for i in range(len(hand_landmarks.landmark)):
@@ -92,9 +94,9 @@ class PythonTest:
                      data_aux.append(x - min(x_))
                      data_aux.append(y - min(y_))
 
+             # If only one hand is detected, duplicate the data_aux
+             data_aux += data_aux
              if num_detected_hands == 1:
-                 # If only one hand is detected, duplicate the data_aux
-                 data_aux += data_aux
 
              x1 = int(min(x_) * W) - 10
              y1 = int(min(y_) * H) - 10
@@ -102,13 +104,20 @@ class PythonTest:
              x2 = int(max(x_) * W) - 10
              y2 = int(max(y_) * H) - 10
 
+             # Run the trained model according to the placements of the landmarks
              prediction = self.model.predict([np.asarray(data_aux)])
+
+             # Declare label of the prediction
              predicted_class_label = prediction[0]
 
+             # Draw rectangle on the detected hand of the frame
              cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+
+             # Write the text of the detected sign language on the frame
              cv2.putText(frame, predicted_class_label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
                          cv2.LINE_AA)
 
+             # Save the frame(=image) back to its directory
              cv2.imwrite("../../resources/captures/output2.jpg", frame)
 
              return str(predicted_class_label) # return predicted character of sign
