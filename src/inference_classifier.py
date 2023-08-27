b18414e0ebadbdc434d7991da5a90d7ea3a57a84
pakinui <poppyschlaadt1999@gmail.com>
2023-08-27 16:38:43 +1200

    Update inference_classifier.py

--- src/inference_classifier.py
+++ src/inference_classifier.py
@@ -8,7 +8,6 @@ import mediapipe as mp
 import numpy as np
 import pickle
 import cv2
-from ctypes import *
 
 ## @brief A class representing sign language detection using webcam and trained model.
 #
@@ -16,7 +15,6 @@ from ctypes import *
 #  using the MediaPipe library for hand tracking and a trained model for sign
 #  language recognition.
 class PythonTest:
-    
     ## @brief Initialize the PythonTest class.
     #
     #  This constructor initializes the necessary components for sign language detection.
@@ -56,77 +54,77 @@ class PythonTest:
     #  @return The predicted sign language character.
     def sign_identifier(self):
         
-         frame = cv2.imread("../../resources/captures/output.jpg")
+        frame = cv2.imread("../../resources/captures/output.jpg")
         
-         ## Declare variable data_aux.
-         data_aux = []
-         x_ = []
-         y_ = []
+        ## Declare variable data_aux.
+        data_aux = []
+        x_ = []
+        y_ = []
 
-         H, W, _ = frame.shape
+        H, W, _ = frame.shape
 
-         ## Modify the cv to be fliped % set frame rgb.
-         frame = cv2.flip(frame, 1)
-         ## Convert the frame to rgb.
-         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        ## Modify the cv to be fliped % set frame rgb.
+        frame = cv2.flip(frame, 1)
+        ## Convert the frame to rgb.
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-         ## Declare variable of the hand which has been detected on the frame.
-         results = self.hands.process(frame_rgb)
+        ## Declare variable of the hand which has been detected on the frame.
+        results = self.hands.process(frame_rgb)
 
         # If statement to place landmarks of fingers(if the hand was detected)
-         if results.multi_hand_landmarks:
-             ## Declare variable num_detected_hands.
-             num_detected_hands = len(results.multi_hand_landmarks)
+        if results.multi_hand_landmarks:
+            ## Declare variable num_detected_hands.
+            num_detected_hands = len(results.multi_hand_landmarks)
 
-             for hand_landmarks in results.multi_hand_landmarks:
-                 for i in range(len(hand_landmarks.landmark)):
-                     ## Declare variable x for hand_landmarks.
-                     x = hand_landmarks.landmark[i].x
-                     ## Declare variable y for hand_landmarks.
-                     y = hand_landmarks.landmark[i].y
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    ## Declare variable x for hand_landmarks.
+                    x = hand_landmarks.landmark[i].x
+                    ## Declare variable y for hand_landmarks.
+                    y = hand_landmarks.landmark[i].y
 
-                     x_.append(x)
-                     y_.append(y)
+                    x_.append(x)
+                    y_.append(y)
 
-                 for i in range(len(hand_landmarks.landmark)):
-                     x = hand_landmarks.landmark[i].x
-                     y = hand_landmarks.landmark[i].y
-                     data_aux.append(x - min(x_))
-                     data_aux.append(y - min(y_))
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
 
-             # If only one hand is detected, duplicate the data_aux
-             if num_detected_hands == 1:
-                 data_aux += data_aux
+            # If only one hand is detected, duplicate the data_aux
+            if num_detected_hands == 1:
+                data_aux += data_aux
 
-             ## Declare variable x1.
-             x1 = int(min(x_) * W) - 10
-             ## Declare variable y1.
-             y1 = int(min(y_) * H) - 10
+            ## Declare variable x1.
+            x1 = int(min(x_) * W) - 10
+            ## Declare variable y1.
+            y1 = int(min(y_) * H) - 10
              
-             ## Declare variable x2.
-             x2 = int(max(x_) * W) - 10
-             ## Declare variable y2.
-             y2 = int(max(y_) * H) - 10
+            ## Declare variable x2.
+            x2 = int(max(x_) * W) - 10
+            ## Declare variable y2.
+            y2 = int(max(y_) * H) - 10
 
-             ## Run the trained model according to the placements of the landmarks
-             prediction = self.model.predict([np.asarray(data_aux)])
+            ## Run the trained model according to the placements of the landmarks
+            prediction = self.model.predict([np.asarray(data_aux)])
 
-             ## Declare label of the prediction.
-             predicted_class_label = prediction[0]
+            ## Declare label of the prediction.
+            predicted_class_label = prediction[0]
 
-             # Draw rectangle on the detected hand of the frame
-             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+            # Draw rectangle on the detected hand of the frame
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
 
-             # Write the text of the detected sign language on the frame
-             cv2.putText(frame, predicted_class_label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+            # Write the text of the detected sign language on the frame
+            cv2.putText(frame, predicted_class_label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
                          cv2.LINE_AA)
 
-             # Save the frame(=image) back to its directory
-             cv2.imwrite("../../resources/captures/output2.jpg", frame)
+            # Save the frame(=image) back to its directory
+            cv2.imwrite("../../resources/captures/output2.jpg", frame)
 
-             return str(predicted_class_label) # return predicted character of sign
+            return str(predicted_class_label) # return predicted character of sign
 
-         return "" # return empty string if there is no hand detected
+        return "" # return empty string if there is no hand detected
 
     #cv2.destroyAllWindows()
 
