64364f6ff91c8ad65a610b7d01ab343bdaf955fa
Padraig Carnegie <padraigcarnegie.pc@gmail.com>
2023-08-27 15:12:50 +1200

    correct python documentation?

--- src/webcam_pattern_detection.py
+++ src/webcam_pattern_detection.py
@@ -1,10 +1,9 @@
-"""
-@file webcam_pattern_detection.py
-@brief Contains the implementation of the ASL detection application using webcam and trained model.
+## @file webcam_pattern_detection.py
+#  @brief Contains the implementation of the ASL detection application using webcam and trained model.
+#
+#This file contains the implementation of an American Sign Language (ASL) detection application
+#using webcam capture, OpenCV for hand tracking, and a trained model for sign language recognition.
 
-This file contains the implementation of an American Sign Language (ASL) detection application
-using webcam capture, OpenCV for hand tracking, and a trained model for sign language recognition.
-"""
 
 # Copyright (C) 2022 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
@@ -22,43 +21,44 @@ from PySide6.QtWidgets import (QApplication, QComboBox, QGroupBox,
                                QHBoxLayout, QLabel, QMainWindow, QPushButton,
                                QSizePolicy, QVBoxLayout, QWidget)
 
-# Load the trained sign language detection model
+## Load the trained sign language detection model
 model_dict = pickle.load(open('ASL_model.p', 'rb'))
+## Extract the trained model from the dictionary
 model = model_dict['model']
 
-
+## Declare variable mp_hands that is an instance of mp.solutions.hands.
 mp_hands = mp.solutions.hands
+## Declare variable mp_drawing that is an instance of mp.solutions.drawing_utils.
 mp_drawing = mp.solutions.drawing_utils
+## Declare variable mp_drawing_styles that is an instance of mp.solutions.drawing_styles.
 mp_drawing_styles = mp.solutions.drawing_styles
 
+## declare variable hands that is an instance of mp_hands.Hands.
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
+## @brief Represents a thread for capturing and processing webcam frames.
+#
+#This class extends QThread to create a separate thread for capturing webcam frames,
+#processing them using hand tracking and a trained model, and updating the display.
 class Thread(QThread):
-    """
-    @brief Represents a thread for capturing and processing webcam frames.
 
-    This class extends QThread to create a separate thread for capturing webcam frames,
-    processing them using hand tracking and a trained model, and updating the display.
-    """
+    
     updateFrame = Signal(QImage)
 
+    ## @brief Initialize the Thread class.
+    #
+    #  This constructor initializes the thread and captures the webcam input.
     def __init__(self, parent=None):
-        """
-        @brief Initialize the Thread class.
-
-        This constructor initializes the thread and captures the webcam input.
-        """
         QThread.__init__(self, parent)
         self.status = True
         self.cap = True
 
+    ## @brief Run the thread.
+    #
+    #  This method runs the thread and captures webcam frames, processes them using
+    #  hand tracking and a trained model, and updates the display.
     def run(self):
-        """
-        @brief Run the thread.
-
-        This method runs the thread and captures webcam frames, processes them using
-        hand tracking and a trained model, and updates the display.
-        """
+        
         self.cap = cv2.VideoCapture(0)
         while self.status:
             data_aux = []
@@ -117,12 +117,11 @@ class Thread(QThread):
             self.updateFrame.emit(scaled_img)
         sys.exit(-1)
 
+## @brief Kill the thread.
+#
+#  This method kills the thread and releases the webcam capture.
 def kill_thread():
-    """
-    @brief Kill the thread.
 
-    This method kills the thread and releases the webcam capture.
-    """
     print("Finishing...")
     button2.setEnabled(False)
     button1.setEnabled(True)
@@ -133,45 +132,49 @@ def kill_thread():
     # Give time for the thread to finish
     time.sleep(1)
 
+## @brief Start the thread.
+#
+#  This method starts the thread and enables the stop button.
 def start():
-    """
-    @brief Start the thread.
 
-    This method starts the thread and enables the stop button.
-    """
     print("Starting...")
     button2.setEnabled(True)
     button1.setEnabled(False)
     th.start()
 
+## @brief Set the image.
+#
+#  This method sets the image to be displayed on the label.
 def set_image(image):
-    """
-    @brief Set the image.
 
-    This method sets the image to be displayed on the label.
-    """
     label.setPixmap(QPixmap.fromImage(image))
 
+## Declare variable app that is an instance of QApplication.
 app = QApplication(sys.argv)
+## Declare variable window that is an instance of QMainWindow.
 window = QMainWindow()
 window.setWindowTitle("American Sign Language Detection")
 window.setGeometry(0, 0, 650, 500)
 
-# Main menu bar
+## Main menu bar
 menu = window.menuBar()
+## menu_file adds the File option to the menu bar.
 menu_file = menu.addMenu("File")
+## Declare variable exit_action that is triggered when the user clicks on the Exit option.
 exit_action = QAction("Exit", window, triggered=QApplication.quit)
 menu_file.addAction(exit_action)
 
+## menu_about adds the About option to the menu bar.
 menu_about = menu.addMenu("&About")
+## Declare variable about_action that is triggered when the user clicks on the About Qt option.
 about_action = QAction("About Qt", window, shortcut=QKeySequence(QKeySequence.HelpContents), triggered=QApplication.aboutQt)
 menu_about.addAction(about_action)
 
-# Create a label for the display camera
+## Create a label for the display camera
 label = QLabel(window)
 label.setFixedSize(640, 480)
 
-# Thread in charge of updating the image
+## Thread in charge of updating the image
 th = Thread(window)
 th.finished.connect(window.close)
 th.updateFrame.connect(set_image)
@@ -190,25 +193,28 @@ th.updateFrame.connect(set_image)
 #model_layout.addWidget(combobox, 90)
 #group_model.setLayout(model_layout)
 
-# Buttons layout
+## Buttons layout that contains the start and stop buttons.
 buttons_layout = QHBoxLayout()
+## Declare variable button1 that is an instance of QPushButton.
 button1 = QPushButton("Start")
+## Declare variable button2 that is an instance of QPushButton.
 button2 = QPushButton("Stop/Close")
 button1.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
 button2.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
 buttons_layout.addWidget(button2)
 buttons_layout.addWidget(button1)
 
+## Right layout that contains the model group and the buttons layout.
 right_layout = QHBoxLayout()
 #right_layout.addWidget(group_model, 1)
 right_layout.addLayout(buttons_layout, 1)
 
-# Main layout
+## Main layout
 layout = QVBoxLayout()
 layout.addWidget(label)
 layout.addLayout(right_layout)
 
-# Central widget
+## Central widget
 widget = QWidget(window)
 widget.setLayout(layout)
 window.setCentralWidget(widget)
