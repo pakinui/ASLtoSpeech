0997c6b0d3b282476f2daf863e8a112cf932a56a
pakinui <poppyschlaadt1999@gmail.com>
2023-09-15 10:06:24 +1200

    update for github actions

---
+++ coverage/generate_coverage_gh.ps1
@@ -0,0 +1,5 @@
+& 'C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe' `
+    --export_type cobertura:coverage\coverage_info.xml `
+    build\345Application.exe `
+    --modules build\345Application.exe `
+    --sources=cosc345\src
---
+++ coverage/generate_coverage_tests_gh.ps1
@@ -0,0 +1,5 @@
+& 'C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe' `
+    --export_type cobertura:coverage\coverage_tests.xml `
+    build\Tests.exe `
+    --modules build\Tests.exe `
+    --sources=cosc345\tests
