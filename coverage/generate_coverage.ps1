7d39995fb095778f084a8a77d5c0f4e222fc4b40
pakinui <poppyschlaadt1999@gmail.com>
2023-09-15 09:56:46 +1200

    hopefully sorted out coverage...
    and updated gitignore

---
+++ coverage/generate_coverage.ps1
@@ -0,0 +1,5 @@
+& 'C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe' `
+    --export_type cobertura:coverage\coverage_info.xml `
+    build\build-cosc345-Desktop_x86_windows_msvc2022_pe_64bit-RelWithDebInfo\345Application.exe `
+    --modules build\build-cosc345-Desktop_x86_windows_msvc2022_pe_64bit-RelWithDebInfo\345Application.exe `
+    --sources=cosc345\src
---
+++ coverage/generate_coverage_tests.ps1
@@ -0,0 +1,5 @@
+& 'C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe' `
+    --export_type cobertura:coverage\coverage_tests.xml `
+    build\build-cosc345-Desktop_x86_windows_msvc2022_pe_64bit-RelWithDebInfo\Tests.exe `
+    --modules build\build-cosc345-Desktop_x86_windows_msvc2022_pe_64bit-RelWithDebInfo\Tests.exe `
+    --sources=cosc345\tests
